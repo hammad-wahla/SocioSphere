a3dd5f50bea34a79133eab663667d775450da72c
hammad-wahla <hammadwahla21@gmail.com>
2025-07-19 12:38:40 +0500

    Update - vercel url added

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 // app.use(cors());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["https://sociosphere-ten.vercel.app", "http://localhost:3000"],
     credentials: true,
   })
 );
